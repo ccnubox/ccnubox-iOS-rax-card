b31432bd14326915bc8bd6a997774945330cfae3
fengzi2016 <1820166258@qq.com>
2018-03-14 20:40:17 +0800

    feat:change fetch url

--- schoolCard/services/Money.js
+++ schoolCard/services/Money.js
@@ -4,22 +4,8 @@ const MoneyService ={
     getMoney(option){
         return request({
             method:"GET",
-            url:"console.ccnu.edu.cn/ecard/getTrans?userId="+option.userId+"&days=1&startNum=0&num=200"
+            url:"http://console.ccnu.edu.cn/ecard/getTrans?userId="+option.userId+"&days=1&startNum=0&num=200"
         })
-        // return fetch('http://console.ccnu.edu.cn/ecard/getTrans?userId="+option.userId+"&days=1&startNum=0&num=200', {
-        //     //mode: 'same-origin',
-        //     dataType: 'json',
-        //     method: 'GET'
-        //   })
-        //   .then((response) => {
-        //     return response.json();
-        //   })
-        //   .then((data) => {
-        //     console.log(data);
-        //   })
-        //   .catch((err) => {
-        //     // handle exception
-        //   });
     }
 }
 export default MoneyService;
