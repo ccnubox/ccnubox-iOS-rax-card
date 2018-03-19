16c2ceaaa73abac545f56807ac3a2586306b5ddc
fengzi2016 <1820166258@qq.com>
2018-03-19 19:59:17 +0800

    fix:add moment

--- schoolCard/src/App.js
+++ schoolCard/src/App.js
@@ -1,23 +1,28 @@
-
-
-
 import {createElement, Component, render} from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
-//import date from './dateFormatter';
-// import styles from './App.css';
+import moment from "moment"
 import MoneyService from '../services/Money.js';
 import Image from 'rax-image';
- const stylechange = 0.56;
+
+const stylechange = 0.56;
+
+
+let image = {
+  uri:"https://wx3.sinaimg.cn/large/006P0MECly1fpflxjkgfdj30fa0jjjr9.jpg"
+};
+
+let date = moment(new Date()).format("YYYY年MM月DD日"); 
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      money : '500.00',
-      time : '2018年3月14日',
+      money : '0.00',
+      time : date,
     }
   }
+ 
   componentWillMount(){
     let option = {};
    // option.userId = Cookie.getCookie("userId");
@@ -25,22 +30,19 @@ class App extends Component {
      MoneyService.getMoney(option)
      .then((res)=>{
        let money = res[0].outMoney;
-       console.log("money:"+money);
        this.setState({money});
      })
-    
-   
   }
+  
 
   render() {
     return (
      
       <View   style={styles.App}>
      
-      <Image  style={styles.centerPicture} source={require('./assets/center-card-picture.png')}
+      <Image  style={styles.centerPicture} source={image}
         resizeMode="contain"
       />
-      {/* <View style={styles.centerPicture}></View> */}
       <Text style={styles.warnWord}>您的校园卡余额是为</Text>
       <Text style={styles.Money}>{this.state.money}元</Text>
       <Text style={styles.Date}>截止至{this.state.time}</Text>
@@ -52,17 +54,17 @@ class App extends Component {
 const styles={
 
   App:{
+   
     backgroundColor:"rgb(239,239,244)",
     width:750,
     height:1334,
     alignItems: 'center'
   },
   centerPicture:{
-   
     width:550,
-    height:704,
-    marginTop: 137,
-    //backgroundColor:'black'
+    height:703,
+    marginTop: 140,
+   
   },
   warnWord:{
     fontSize:34,
